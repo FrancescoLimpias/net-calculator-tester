7ded846de3eecf0c33e6c58ac132eb4bf8859a6e
Francesco Limpias <francescolimpias@gmail.com>
2023-04-26 16:14:51 +0200

    Added Power function and UnitTest for Exponentiation

--- CalculatorUnitTest/UnitTest1.cs
+++ CalculatorUnitTest/UnitTest1.cs
@@ -61,5 +61,27 @@ namespace CalculatorUnitTest
             Assert.That(Calculator.Multiply(numberA, numberB), Is.EqualTo((float)expectedResult), $"Multiplication has unexpected result: {numberA} * {numberB} ");
         }
 
+
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, 0)]
+        [TestCase(0, 2, 0)]
+
+        [TestCase(1, 0, 1)]
+        [TestCase(1, -1, 1)]
+        [TestCase(-1, -1, -1)]
+
+        [TestCase(20, 400, Double.PositiveInfinity)]
+        [TestCase(400, 20, Double.PositiveInfinity)]
+        [Test(Description = "Test for calculator' exponentiation")]
+        public void TestPower(float numberA, float numberB, double expectedResult)
+        {
+            Assume.That(
+                !(numberA == 0.0
+                && numberB == 0.0),
+                "Base and exponent are 0! -> invalid operation");
+
+            Assert.That(Calculator.Power(numberA, numberB), Is.EqualTo((double)expectedResult), $"Exponentiation has unexpected result: {numberA} ^ {numberB} ");
+        }
+
     }
 }
--- net-calculator-tester/Calculator.cs
+++ net-calculator-tester/Calculator.cs
@@ -21,5 +21,28 @@ namespace net_calculator_tester
         public static float Multiply(float a, float b)
         { return a * b; }
 
+        public static double Power(float b, float a)
+        {
+            if (a == 0)
+            {
+                return 1;
+            }
+
+            double result = Power(b, a / 2);
+
+            if (a % 2 == 0)
+            {
+                return result * result;
+            }
+            else if (a > 0)
+            {
+                return b * result * result;
+            }
+            else
+            {
+                return (result * result) / b;
+            }
+        }
+
     }
 }
