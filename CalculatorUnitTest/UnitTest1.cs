04472ec98872e3db6c3640457d9fd4146b3e6ebc
Francesco Limpias <francescolimpias@gmail.com>
2023-04-26 15:38:56 +0200

    Added UnitTest for Multiplication

--- CalculatorUnitTest/UnitTest1.cs
+++ CalculatorUnitTest/UnitTest1.cs
@@ -41,8 +41,20 @@ namespace CalculatorUnitTest
         [Test(Description = "Test for calculator' division")]
         public void TestDivide(float numberA, float numberB, float expectedResult)
         {
-            Assume.That(numberB != 0.0, "The dividend is 0");
+            Assume.That(numberB != 0.0, "The dividend is 0 -> invalid operation");
             Assert.That(Calculator.Divide(numberA, numberB), Is.EqualTo((float)expectedResult), $"Division has unexpected result: {numberA} / {numberB} ");
+        }
+
+
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, 0)]
+        [TestCase(1, 0, 0)]
+        [TestCase(1, -1, -1)]
+        [TestCase(20, 400, 8000)]
+        [Test(Description = "Test for calculator' multiplication")]
+        public void TestMultiply(float numberA, float numberB, float expectedResult)
+        {
+            Assert.That(Calculator.Multiply(numberA, numberB), Is.EqualTo((float)expectedResult), $"Multiplication has unexpected result: {numberA} * {numberB} ");
         }
 
     }
