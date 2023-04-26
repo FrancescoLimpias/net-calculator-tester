80451c218091d0a0e7b4c15355359defd1e5c201
Francesco Limpias <francescolimpias@gmail.com>
2023-04-26 15:23:53 +0200

    Added UnitTest for Subtraction

--- CalculatorUnitTest/UnitTest1.cs
+++ CalculatorUnitTest/UnitTest1.cs
@@ -9,16 +9,29 @@ namespace CalculatorUnitTest
         {
         }
 
+
         [TestCase(0, 0, 0)]
         [TestCase(0, 1, 1)]
         [TestCase(1, 0, 1)]
         [TestCase(1, -1, 0)]
         [TestCase(20, 400, 420)]
-        [Test(Description = "Test for add calculator")]
+        [Test(Description = "Test for calculator's addition")]
         public void TestAdd(float numberA, float numberB, float expectedResult)
         {
-
             Assert.That(Calculator.Add(numberA, numberB), Is.EqualTo(expectedResult), $"Addition has unexpected result: {numberA} + {numberB} ");
         }
+
+
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, -1)]
+        [TestCase(1, 0, 1)]
+        [TestCase(1, -1, 2)]
+        [TestCase(20, 400, -380)]
+        [Test(Description = "Test for calculator' subtraction")]
+        public void TestSubtract(float numberA, float numberB, float expectedResult)
+        {
+            Assert.That(Calculator.Subtract(numberA, numberB), Is.EqualTo((float)expectedResult), $"Subtraction has unexpected result: {numberA} - {numberB} ");
+        }
+
     }
 }
