85b11ef6df836648d76700ca92ba6450653789fa
Francesco Limpias <francescolimpias@gmail.com>
2023-04-26 15:35:11 +0200

    Added UnitTest for Division

--- CalculatorUnitTest/UnitTest1.cs
+++ CalculatorUnitTest/UnitTest1.cs
@@ -33,5 +33,17 @@ namespace CalculatorUnitTest
             Assert.That(Calculator.Subtract(numberA, numberB), Is.EqualTo((float)expectedResult), $"Subtraction has unexpected result: {numberA} - {numberB} ");
         }
 
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, 0)]
+        [TestCase(1, 0, 0)]
+        [TestCase(1, -1, -1)]
+        [TestCase(20, 400, (float)0.05)]
+        [Test(Description = "Test for calculator' division")]
+        public void TestDivide(float numberA, float numberB, float expectedResult)
+        {
+            Assume.That(numberB != 0.0, "The dividend is 0");
+            Assert.That(Calculator.Divide(numberA, numberB), Is.EqualTo((float)expectedResult), $"Division has unexpected result: {numberA} / {numberB} ");
+        }
+
     }
 }
